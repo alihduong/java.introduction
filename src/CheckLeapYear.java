751c0d22f544a31b42c1faa0f6036af0202821e7
alihduong <nguyenhoangduong0711@gmail.com>
2021-12-06 01:25:47 +0700

    add file

--- src/CheckLeapYear.java
+++ src/CheckLeapYear.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CheckLeapYear {
+import java.util.Scanner;
+
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int years;
+        System.out.print("Enter Years");
+        years = scanner.nextInt();
+        if(years % 4 == 0){
+            if(years % 100 == 0){
+                if(years % 400 == 0){
+                    System.out.printf("%d is a leap year", years);
+                } else {
+                    System.out.printf("%d is NOT a leap year", years);
+                }
+            } else {
+                System.out.printf("%d is a leap year", years);
+            }
+        } else {
+            System.out.printf("%d is NOT a leap year", years);
+        }
+    }
 }
